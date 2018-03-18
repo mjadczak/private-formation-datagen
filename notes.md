51b66fbe0660830b0a7491e7321f7f40e68c2014
Matt Jadczak <matt@mjdk.co.uk>
2018-03-18 21:31:42 +0000

    Add notes and argument parsing

---
+++ notes.md
@@ -0,0 +1,5 @@
+## Sensor noise equation
+Cubic, no bias, coefficients:
+```
+p = -0.020077009250469, 0.120573832696841, 0.003295559781587
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[macro_use]
+extern crate clap;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap_app!(datagen =>
+        (version: "0.1")
+        (author: "Matt Jadczak <mnj24@cam.ac.uk>")
+        (about: "Generates trajectory data for simulated robots")
+    ).get_matches();
 }
